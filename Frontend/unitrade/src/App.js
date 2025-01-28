f149342f0e58da36e13bcd1e297ca9877d33719f
shubham1698 <shubham.m.singh@gmail.com>
2025-01-28 11:09:48 -0500

    LoginPage UI

--- Frontend/unitrade/src/App.js
+++ Frontend/unitrade/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from './pages/LandingPage';
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <Banner />
+      <LoginPage />
     </div>
   );
 }
