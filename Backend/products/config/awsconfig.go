d03f08bb4770e2d4b0bdeef47bac644df82c50cd
AvaneeshKhandekar <avaneesh.khandekar@gmail.com>
2025-02-20 01:48:14 -0500

    Debug method commented

--- Backend/products/config/awsconfig.go
+++ Backend/products/config/awsconfig.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -13,9 +12,10 @@ var awsClientObj *s3.Client
 
 func loadAWSConfig() *s3.Client {
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithClientLogMode(aws.LogRequest|aws.LogResponse|aws.LogRetries),
-	)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	// Uncomment to debug
+	// config.WithClientLogMode(aws.LogRequest|aws.LogResponse|aws.LogRetries),
+
 	if err != nil {
 		log.Printf("error: %v", err)
 		return nil
