5719a76223f26cc79fc0c01281911e528e81e019
shubham1698 <shubham.m.singh@gmail.com>
2025-02-22 22:21:08 -0500

    Docker File create for frontend

--- FE-UniBazaar/vite.config.js
+++ FE-UniBazaar/vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
+// Detect if running inside Docker
+const isDocker = process.env.DOCKER === "true";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      "@": "/src", // This resolves @ to the src directory
+      "@": "/src",
     },
   },
   server: {
     port: 3000,
-    open: true,
+    open: !isDocker, // Open in local dev, not in Docker
+    host: "0.0.0.0",  // Ensures Docker works properly
   },
 });
