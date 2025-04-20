80a7ddc12fd3fe932bf97e5428e6812d6b58176a
Sakshi <svpandey97@gmail.com>
2025-04-20 17:07:29 -0400

    fixed CORS issue, updated banner and frontpage

--- Backend/users/server/server.go
+++ Backend/users/server/server.go
@@ -2,23 +2,52 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
 	config "users/config"
 	handler "users/handler"
 	models "users/models"
+
+	"github.com/joho/godotenv" // go get github.com/joho/godotenv
 )
 
 func InitServer() {
-	dsn := "postgresql://user:npg_8eILQzF2cGhM@ep-damp-bread-a88f66ei-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
-	app := handler.Application{}
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found relying on real environment variables")
+	}
+
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		log.Fatal("DSN environment variable not set")
+	}
+
 	conn := config.Connect(dsn)
-	app.Models = models.NewModels(conn)
+
+	app := handler.Application{
+		Models: models.NewModels(conn),
+	}
+
 	fmt.Println("connected to database")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	srv := http.Server{
-		Addr:    ":4000",
+		Addr:    ":" + port,
 		Handler: app.Routes(),
 	}
-	fmt.Printf("app running on port %d", 4000)
-	_ = srv.ListenAndServe()
+
+	//	srv := http.Server{
+	//		Addr:    ":8080",
+	//		Handler: app.Routes(),
+	//}
+
+	fmt.Println("app running on port 8080")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
